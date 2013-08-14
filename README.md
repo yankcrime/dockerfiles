f9e189145acbd70e2338e5278daacd3043aea66c
Nick Jones <nick@dischord.org>
2013-08-14 23:30:04 +0100

    Minor fixes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dockerfiles
-See http://docker.io
+For use with [Docker](http://docker.io)
+See also:
+	http://dischord.org/blog/2013/07/10/docker-and-owncloud/
+	http://dischord.org/blog/2013/08/13/docker-and-owncloud-part-2/
+
--- owncloud/resources/cfgmysql.sh
+++ owncloud/resources/cfgmysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 /usr/sbin/mysqld &
 sleep 5
-/usr/bin/mysql -u root -proot123 -e "CREATE DATABASE owncloud; GRANT ALL ON owncloud.* TO 'owncloud'@'localhost' IDENTIFIED BY 'owncloudsql'; FLUSH PRIVILEGES;"
+/usr/bin/mysql -u root -proot123 -e "CREATE DATABASE owncloud; GRANT ALL ON owncloud.* TO 'owncloud'@'localhost' IDENTIFIED BY 'owncloudsql';"
 pkill -f mysqld
